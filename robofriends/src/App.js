52dac7ab89ba056df8e147e9570532fb71e90e7a
thekaranjit <thekaranjit@gmail.com>
2018-08-05 03:52:13 +0400

    Copleted with state update error.. Need to work

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,21 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CardList from'./CardList';
+import SearchBox from './SearchBox';
+import './App.css'
+
+
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+	constructor() {
+		super()
+		this.state = {
+
+			robots: [],
+			searchfield: ''
+
+
+		}
+		
+	}
+
+componentDidMount(){
+	fetch('https://jsonplaceholder.typicode.com/users')
+
+	.then(response=> response.json())
+	.then(users=> this.setState({robots: users}));
+	
 }
+
+
+
+	onSearchChange = (event) => {
+
+		this.setState({ searchField: event.target.value })
+
+		
+		
+	}
+	render(){
+
+		const filteredRobots = this.state.robots.filter(robots =>{
+
+			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+
+		})
+
+		if (this.state.robots.length ===0) {
+			return <h1> Loding </h1>
+		}	
+
+		else {
+
+		return (
+
+			<div className='tc'>
+
+				<h1 className='f1'>RoboFriends</h1>
+				<SearchBox searchChange={this.onSearchChange}/>
+				<CardList  robots={filteredRobots}/>
+
+			</div>
+			);
+
+
+	}
+
+	
+}
+
+}
+
 
 export default App;
--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import App from './App'
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 
-ReactDOM.render(<Hello greeting={'Hello' + 'React Ninja'} />, document.getElementById('root'));
+import CardList from './CardList'
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
